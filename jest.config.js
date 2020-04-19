369cb12a38d2272d3cb5e22aaf8893f9e788123d
Tyler Hawkins <tylerhawkins@workfront.com>
2020-04-19 14:09:46 -0600

    test: renames test directory to __tests__

--- jest.config.js
+++ jest.config.js
@@ -147,13 +147,13 @@ module.exports = {
   // The glob patterns Jest uses to detect test files
   testMatch: [
     "**/__tests__/**/*.[jt]s?(x)",
-    "**/?(*.)+(spec|test).[tj]s?(x)"
   ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   testPathIgnorePatterns: [
     "/node_modules/",
-    "/cypress/"
+    "/cypress/",
+    "/.cache"
   ],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
